c2d507951fe7b67ba9f6ff63daaab4eebcd06e39
CaseyHackerMan <60766187+CaseyHackerMan@users.noreply.github.com>
2021-07-22 08:38:51 -0700

    Revert "code works"
    This reverts commit bb59c37cd1b6656511c5519ff3469d1a6e6e5145.

--- src/main/java/frc/robot/SwerveModule.java
+++ src/main/java/frc/robot/SwerveModule.java
@@ -8,17 +8,16 @@
 package frc.robot;
 
 import com.revrobotics.CANAnalog;
+import com.revrobotics.CANEncoder;
 import com.revrobotics.CANPIDController;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.ControlType;
+import com.revrobotics.EncoderType;
 import com.revrobotics.CANAnalog.AnalogMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
-import edu.wpi.first.wpilibj.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.geometry.Translation2d;
 import edu.wpi.first.wpilibj.kinematics.SwerveModuleState;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpiutil.math.MathUtil;
 
 /**
  * Add your docs here.
@@ -32,15 +31,11 @@ public class SwerveModule {
 
     private final CANAnalog spinAnalogEncoder;
     private final CANPIDController spinPIDController;
-
-    private final double offset;
     
-    public SwerveModule(int driveMotorId, int spinMotorId, Translation2d location, double offset_) {
+    public SwerveModule(int driveMotorId, int spinMotorId, Translation2d location) {
         this.location = location;
         driveMotor = new CANSparkMax(driveMotorId, MotorType.kBrushless);
         spinMotor = new CANSparkMax(spinMotorId, MotorType.kBrushless);
-
-        offset = offset_;
 
         spinAnalogEncoder = spinMotor.getAnalog(AnalogMode.kAbsolute);
         spinAnalogEncoder.setPositionConversionFactor(1 / 3.3);
@@ -73,10 +68,6 @@ public class SwerveModule {
         spinAnalogEncoder.setInverted(inverted);
     }
 
-    public void setDriveMotorInverted(boolean inverted) {
-        driveMotor.setInverted(inverted);
-    }
-
     public void setSpinPIDConstants(double kP, double kI, double kD) {
         spinPIDController.setP(kP);
         spinPIDController.setI(kI);
@@ -86,45 +77,19 @@ public class SwerveModule {
     // DON'T USE THIS YET. IT ISN'T TESTED.
     // WE ALSO NEED TO ADD THE ACTUAL DRIVE WHEEL MOTORS
     public void setModuleState(SwerveModuleState state) {
-        if(state.angle.getDegrees() == 0 && state.speedMetersPerSecond == 0) {
-            driveMotor.set(0);
-            return;
-        }
-        //if(driveMotor.getDeviceId() != 7) return;
         // Desired angle (in degrees)
         double angle = state.angle.getDegrees();
-        if(angle < 0) angle += 360;
-        //angle = MathUtil.clamp(angle, 30, 330);
-        double rot = angle / 360;
-        boolean invertDrive = false;
-        if(rot < 0.08) {
-            rot += 0.5;
-            invertDrive = true;
-        }
-        if(rot > 0.92) {
-            rot -= 0.5;
-            invertDrive = true;
-        }
-        
-        spinPIDController.setReference(rot, ControlType.kPosition);
-
-        //SmartDashboard.putNumber("SPINNY SPINNY!", state.speedMetersPerSecond);
-
-        // TODO Should be done with PID and velocity instead
-        double driveSpeed = state.speedMetersPerSecond;
-        driveSpeed /= 4;
-        if(invertDrive) driveSpeed *= -1;
+        spinPIDController.setReference(angle / 360, ControlType.kPosition);
 
         // TODO Temp
-        //driveMotor.set(state.speedMetersPerSecond / 6.0);
-        driveMotor.set(driveSpeed);
-        //spinPIDController.setReference(0.30, ControlType.kPosition);
+        driveMotor.set(state.speedMetersPerSecond / 6.0);
     }
 
 
 
-    public double correct(double cons) {
+    public double correct(double cons){
         double start = (spinAnalogEncoder.getVoltage() / 3.3);
+
         return (start - cons) % 1;
     }
 }
